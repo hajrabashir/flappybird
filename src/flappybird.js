7b5ab217ce321b826524009e63c1c752b6814cca
hajra bashir <hajrabashir69@gmail.com>
2025-10-21 19:08:33 +0200

    added comment

--- src/flappybird.js
+++ src/flappybird.js
@@ -1,44 +1,53 @@
-
 let board;
-let boardHeight=640;
-let boardWidth=360;
+let boardHeight = 640;
+let boardWidth = 360;
 let context;
 
-let birdWidth= 34;
-let birdHeight= 24;
-let birdX= boardWidth /8;
-let birdY= (boardHeight / 2) - (birdHeight / 2);
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2 - birdHeight / 2;
 let birdImg;
 
-let pipeWidth= 50;
+let pipeWidth = 50;
 let pipeHeight = 512;
 let pipeX = 200;
 let pipeY = 0;
-
+//yyyyy
 let topPipeImg;
 let bottompipeImg;
- 
-window.onload = function(){
+
+window.onload = function () {
   board = document.getElementById("board");
-board.height = boardHeight;
-board.width = boardWidth;
-context = board.getContext("2d");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d");
 
-birdImg = new Image();
-birdImg.src= "./flappybird.png";
+  birdImg = new Image();
+  birdImg.src = "./flappybird.png";
   topPipeImg = new Image();
-  topPipeImg.src= "./toppipe.png";
+  topPipeImg.src = "./toppipe.png";
   bottompipeImg = new Image();
-  bottompipeImg.src= "./bottompipe.png";
-birdImg.onload = function(){
-  context.drawImage(birdImg, birdX, birdY, birdWidth, birdHeight);
+  bottompipeImg.src = "./bottompipe.png";
+  birdImg.onload = function () {
+    context.drawImage(birdImg, birdX, birdY, birdWidth, birdHeight);
+  };
+  topPipeImg.onload = function () {
+    context.drawImage(
+      topPipeImg,
+      pipeX,
+      pipeY - pipeHeight / 4,
+      pipeWidth,
+      pipeHeight
+    );
+  };
+  bottompipeImg.onload = function () {
+    context.drawImage(
+      bottompipeImg,
+      pipeX,
+      pipeY + pipeHeight / 2 + 250,
+      pipeWidth,
+      pipeHeight
+    );
+  };
 };
-  topPipeImg.onload = function(){
-  context.drawImage(topPipeImg,   pipeX ,pipeY- pipeHeight / 4,  pipeWidth,  pipeHeight);
-};
-  bottompipeImg.onload = function(){
-  context.drawImage(bottompipeImg, pipeX, pipeY + pipeHeight / 2 + 250, pipeWidth, pipeHeight);
-};
-
-};
-
