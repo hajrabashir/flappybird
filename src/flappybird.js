0c0d25036de1602dd954dea0cb70321c1391138d
hajra bashir <hajrabashir69@gmail.com>
2025-11-05 19:28:37 +0100

    add bird move and game over message

--- src/flappybird.js
+++ src/flappybird.js
@@ -6,17 +6,14 @@ let context;
 let birdWidth = 34;
 let birdHeight = 24;
 let birdX = boardWidth / 8;
-let birdY = boardHeight / 2 ;
+let birdY = boardHeight / 2;
 let birdImg;
-
 
 let velocityY = 0;
 let gravity = 0.1;
 let jumpower = -3;
 
 let gameState = "RUNNING";
-
-
 
 window.onload = function () {
   board = document.getElementById("board");
@@ -27,77 +24,50 @@ window.onload = function () {
   birdImg = new Image();
   birdImg.src = "./flappybird.png";
   birdImg.onload = function () {
-    context.drawImage(birdImg, birdX,birdY,birdWidth,birdHeight)
+    context.drawImage(birdImg, birdX, birdY, birdWidth, birdHeight);
   };
-    requestAnimationFrame(update);
-  
-
-document.addEventListener("keydown", moveBird);
-document.addEventListener("click", moveBird);
-document.addEventListener("keydown",restartGame);
-
-};
-    
-  
-  function update() {
   requestAnimationFrame(update);
-  context.clearRect(0,0, board.width,board.height);
-  if(gameState === "RUNNING") {
 
-  velocityY += gravity;
-  birdY += velocityY;
-  if (birdY + birdHeight > boardHeight)
-     {
+  document.addEventListener("keydown", moveBird);
+  document.addEventListener("click", moveBird);
+  document.addEventListener("keydown", restartGame);
+};
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+  if (gameState === "RUNNING") {
+    velocityY += gravity;
+    birdY += velocityY;
+    if (birdY + birdHeight > boardHeight) {
       birdY = boardHeight - birdHeight;
-      gameState ="GAME_OVER";
-    
-  }
-  if(birdY < 0) {
-    birdY = 0;
-    gameState= 'GAME_OVER';
-  }
-context.drawImage(birdImg, birdX,birdY,birdWidth,birdHeight);
-  }
-  else if (gameState === "GAME_OVER"){
+      velocityY = gravity
+      gameState = "GAME_OVER";
+    }
+    if (birdY < 0) {
+      birdY = 0;
+      velocityY = gravity
+      gameState = "GAME_OVER";
+    }
+    context.drawImage(birdImg, birdX, birdY, birdWidth, birdHeight);
+  } else if (gameState === "GAME_OVER") {
     context.fillStyle = "red";
     context.font = "bold 32px Arial";
-    context.fillText("GAME OVER", 80,250);
-    context.fillStyle="White";
+    context.fillText("GAME OVER", 80, 250);
+    context.fillStyle = "White";
     context.font = "16px Arial";
-    context.fillText ("press R to Restart",110,280);
-
+    context.fillText("press R to Restart", 110, 280);
   }
 }
 function moveBird(e) {
-  if (gameState === "RUNNING" && (e.code === "Space"|| e.type === "Click")){
+  if (gameState === "RUNNING" && (e.code === "Space" || e.type === "Click")) {
     velocityY = jumpower;
   }
 }
 function restartGame(e) {
-  if(gameState === "GAME_OVER" && e.code === "KeyR"){
-  birdY = boardHeight /2;
-  velocity = 0;
-  gameState = "RUNNING";
+  if (gameState === "GAME_OVER" && e.code === "KeyR") {
+    birdY = boardHeight / 2;
+    velocity = 0;
+    gameState = "RUNNING";
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
